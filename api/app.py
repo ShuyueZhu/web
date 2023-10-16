14c523a52ccc26a1d73c7db568f2c80212cf8f6a
Shuyue Zhu <sz2223@imperial.ac.uk>
2023-10-16 17:42:54 +0100

    3

--- api/app.py
+++ api/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+@app.route("/")
+def submit():
+    return render_template("index.html")
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
