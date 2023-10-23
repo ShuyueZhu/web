b0474c6c04660f5c5dd0b651ef2c3e6d58b3d043
Shuyue Zhu <sz2223@imperial.ac.uk>
2023-10-23 15:45:16 +0100

    fir

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
