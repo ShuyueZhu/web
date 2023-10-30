fa5b52f489c85ec1d3f9cf906b3699921fe6a6ea
Shuyue Zhu <sz2223@imperial.ac.uk>
2023-10-30 18:34:01 +0000

    42

--- api/app.py
+++ api/app.py
@@ -28,47 +28,48 @@ def process_query(q):
     # prime_pattern = r'Which of the following numbers are primes: ' + \
     #     r'(\d+), (\d+), (\d+), (\d+), (\d+)?'
     op = re.search(r'(plus|minus|multipled|largest|primes|square)', q).group()
-    nums = list(map(int, re.findall(r'\d+', q)))
+    if op:
+        nums = list(map(int, re.findall(r'\d+', q)))
 
-    # match_addition = re.search(addition_pattern, q)
-    if op == 'plus':
-        return str(sum(nums))
+        # match_addition = re.search(addition_pattern, q)
+        if op == 'plus':
+            return str(sum(nums))
 
-    # match_multiplication = re.search(multiplication_pattern, q)
-    if op == 'multipled':
-        return str(nums[0] * nums[1])
+        # match_multiplication = re.search(multiplication_pattern, q)
+        if op == 'multipled':
+            return str(nums[0] * nums[1])
 
-    # match_largest = re.search(largest_pattern, q)
-    if op == 'largest':
-        return str(max(nums))
+        # match_largest = re.search(largest_pattern, q)
+        if op == 'largest':
+            return str(max(nums))
 
-    # match_square_cube = re.search(square_cube_pattern, q)
-    if op == 'square':
-        for num in nums:
-            if math.isqrt(num)**2 == num and round(num**(1/3))**3 == num:
-                return str(num)
+        # match_square_cube = re.search(square_cube_pattern, q)
+        if op == 'square':
+            for num in nums:
+                if math.isqrt(num)**2 == num and round(num**(1/3))**3 == num:
+                    return str(num)
 
-    # match_minus = re.search(minus_pattern, q)
-    if op == 'minus':
-        return str(nums[0] - nums[1])
+        # match_minus = re.search(minus_pattern, q)
+        if op == 'minus':
+            return str(nums[0] - nums[1])
 
-    # match_prime = re.search(prime_pattern, q)
-    if op == 'primes':
-        results = []
+        # match_prime = re.search(prime_pattern, q)
+        if op == 'primes':
+            results = []
 
-        def is_prime(n):
-            if n <= 1:
-                return False
-            for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
+            def is_prime(n):
+                if n <= 1:
                     return False
-            return True
-        for num in nums:
-            if is_prime(num):
-                results.append(num)
-        return results
+                for i in range(2, int(n**0.5) + 1):
+                    if n % i == 0:
+                        return False
+                return True
+            for num in nums:
+                if is_prime(num):
+                    results.append(num)
+            return results
 
-    if q == "dinosaurs":
+    elif q == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
     elif q == "asteroids":
         return "Unknown"
