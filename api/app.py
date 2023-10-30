3970244874c14ba698b594d22b8a5101f3f035f3
Shuyue Zhu <sz2223@imperial.ac.uk>
2023-10-30 17:01:18 +0000

    23

--- api/app.py
+++ api/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, re, math
 app = Flask(__name__)
-
+addition_pattern = r'What is (\d+) plus (\d+)?'
+multiplication_pattern = r'What is (\d+) multiplied by (\d+)? '
+largest_pattern = r'Which of the following numbers is both a square \
+and a cube: (\d+), (\d+), (\d+)?'
+square_cube_pattern = r'Which of the following numbers is the \
+largest: (\d+), (\d+), (\d+)?'
 
 @app.route("/")
 def hello():
@@ -15,17 +20,34 @@ def submit():
 
 
 def process_query(q):
+    match_addition = re.match(addition_pattern, q)
+    if match_addition:
+        num1, num2 = map(int, match_addition.groups())
+        return num1+num2
+    
+    match_largest = re.match(largest_pattern, q)
+    if match_largest:
+        num1, num2 = map(int, match_largest.groups())
+        return num1*num2
+    
+    match_square_cube = re.match(square_cube_pattern, q)
+    if match_square_cube:
+        nums = map(int, match_square_cube.groups())
+        result = []
+        for num in nums:
+            if math.isqrt(num)**2 == num and round(num**(1/3))**3 == num:
+                result.append(num)
+        if len(result) == 1:
+            return result[0]
+        else:
+            return result
+    
     if q == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
     elif q == "asteroids":
         return "Unknown"
     elif q == "What is your name?":
         return "SY"
-    elif q == r'What is (\d+) plus (\d+)?':
-        input = q.split("plus")
-        num1 = int(input[0].split("is")[1].strip())
-        num2 = int(input[1].strip())
-        return num1+num2
     else:
         return "Unrecognized input!!!"
 
