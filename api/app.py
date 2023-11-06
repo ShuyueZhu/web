efd90777cf0d2b8c6db934d03268bec99c8da91c
Shuyue Zhu <sz2223@imperial.ac.uk>
2023-11-06 16:17:30 +0000

    61

--- api/app.py
+++ api/app.py
@@ -34,55 +34,55 @@ def process_query(q):
         r'(\d+), (\d+), (\d+), (\d+), (\d+)?'
     op = re.search(r'(plus|minus|multiplied|largest|primes|square)', q)
     if op:
-       op = op.group()
-       nums = list(map(int, re.findall(r'\d+', q)))
+        op = op.group()
+        nums = list(map(int, re.findall(r'\d+', q)))
 
-       match_addition = re.search(addition_pattern, q)
-       if op == 'plus':
-          return str(sum(nums))
+        match_addition = re.search(addition_pattern, q)
+        if op == 'plus':
+            return str(sum(nums))
 
-       match_multiplication = re.search(multiplication_pattern, q)
-       if op == 'multiplied':
-           return str(nums[0] * nums[1])
+        match_multiplication = re.search(multiplication_pattern, q)
+        if op == 'multiplied':
+            return str(nums[0] * nums[1])
 
-       match_largest = re.search(largest_pattern, q)
-       if op == 'largest':
-           return str(max(nums))
+        match_largest = re.search(largest_pattern, q)
+        if op == 'largest':
+            return str(max(nums))
 
-       match_square_cube = re.search(square_cube_pattern, q)
-       if op == 'square':
+        match_square_cube = re.search(square_cube_pattern, q)
+        if op == 'square':
 
-           def is_square_and_cube(n):
-               root = round(n**(1/6))
-               return root**6 == n
-           result_numbers = [num for num in nums if is_square_and_cube(num)]
-           return ', '.join(map(str, result_numbers))
+            def is_square_and_cube(n):
+                root = round(n**(1/6))
+                return root**6 == n
+            result_numbers = [num for num in nums if is_square_and_cube(num)]
+            return ', '.join(map(str, result_numbers))
 
-       match_minus = re.search(minus_pattern, q)
-       if op == 'minus':
-           return str(nums[0] - nums[1])
+        match_minus = re.search(minus_pattern, q)
+        if op == 'minus':
+            return str(nums[0] - nums[1])
 
-       match_prime = re.search(prime_pattern, q)
-       if op == 'primes':
+        match_prime = re.search(prime_pattern, q)
+        if op == 'primes':
 
-           def is_prime(n):
-               if n <= 1:
-                   return False
-               for i in range(2, int(n**0.5) + 1):
-                   if n % i == 0:
-                       return False
-               return True
-           primes = [str(num) for num in nums if is_prime(num)]
-           return ', '.join(primes)
+            def is_prime(n):
+                if n <= 1:
+                    return False
+                for i in range(2, int(n**0.5) + 1):
+                    if n % i == 0:
+                        return False
+                return True
+            primes = [str(num) for num in nums if is_prime(num)]
+            return ', '.join(primes)
 
-    elif q == "dinosaurs":
-       return "Dinosaurs ruled the Earth 200 million years ago"
-    elif q == "asteroids":
-       return "Unknown"
-    elif q == "What is your name?":
-       return "ZSY"
-    else:
-       return "Unrecognized input!!!"
+        elif q == "dinosaurs":
+            return "Dinosaurs ruled the Earth 200 million years ago"
+        elif q == "asteroids":
+            return "Unknown"
+        elif q == "What is your name?":
+            return "ZSY"
+        else:
+            return "Unrecognized input!!!"
 
 
 @app.route('/query', methods=['GET'])
